a507f73926f8b125c719948274fe5060f1c5286f
Vaibhav Panvalkar <user@Users-MacBook-Pro.local>
2022-02-07 20:53:53 +0530

    basic string

---
+++ Strings/string0.py
@@ -0,0 +1,14 @@
+my_string = "India"
+
+# getting first letter from string
+print(my_string[0])
+
+# getting last letter
+print(my_string[-1])
+print(my_string[len(my_string)-1])
+
+# getting substring from index 1 till 3 but not included 3
+print(my_string[1:3])
+
+# string reverse print
+print(my_string[::-1])
---
+++ Strings/string1.py
@@ -0,0 +1,23 @@
+# string immutablity
+name = "Sam"
+# name[0] = 'P' # this will throw an err as strings are immutable
+name = "P" + name[1:] # this works as redefined value for name
+# print(name)
+
+# repeat strings
+snore = 'z' * 10
+print(snore)
+
+
+# methods on strings
+
+hello = 'Hello World'
+
+# upper case
+# print(hello.upper())
+
+# lower case 
+print(hello.lower())
+
+# split string => using white space
+print(hello.split())
