534e1570d66113e3911709ac932e44d1152258d1
Vaibhav Panvalkar <user@Users-MacBook-Pro.local>
2022-02-08 11:52:27 +0530

    string formating

---
+++ Strings/string2.py
@@ -0,0 +1,18 @@
+# string iterpolation using format method
+name = "vaibhav"
+country = "India"
+age = 27
+print("Hello {}".format(name))
+
+# using positions
+print("Name:{2}, Country:{0},age:{1}".format(country,age,name))
+
+# using variables
+print("The {q} {b} {f}".format(q='Quick',b='Brown',f='Fox'))
+
+# float formating {value:width.precision f}
+result = 22/7
+print("The result is {r:1.3f}".format(r=result))
+
+# another way to format
+print(f"New result {result:1.2f}")
