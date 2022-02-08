71f84694d6fe45a9957746090cc347d0df5945aa
Vaibhav Panvalkar <vpanvalkar@outlook.com>
2022-02-08 16:14:53 +0530

    comparision operators

---
+++ Comparision/compare0.py
@@ -0,0 +1,9 @@
+print(2 == 2) # equality
+print('hello' == 'Hello') 
+print('2' == 2)
+print(2.1 == 2.10) # this is tricky
+print((1.1 + 1) == 2.10)
+print(2.1 > 2)
+print(2.1 >= 2)
+print(2.1 != 2)
+print(2.1 < 2)
---
+++ Comparision/compare1.py
@@ -0,0 +1,11 @@
+# print(2 < 3)
+print(2 < 3 < 4)
+print(2 < 3 > 4)
+print(2 <= 3 >= 1)
+
+print( 'h' == 'h' and 2 == 2)
+print( 'h' == 'h' and 2 != 2)
+print( 'h' == 'h' or 2 == 2)
+print( 'h' == 'h' or 2 != 2)
+
+print( not 1 == 1)
