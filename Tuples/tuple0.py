600b5634f2eab54312ddfe6dab9c960defb922df
Vaibhav Panvalkar <vpanvalkar@outlook.com>
2022-02-08 14:33:06 +0530

    Tuples basic

---
+++ Tuples/tuple0.py
@@ -0,0 +1,17 @@
+# create simple tuple
+a = ('a', 'a', 'a')
+
+# print tuple
+print(a)
+
+# count
+print(a.count('a')) # 3
+
+# index
+print(a.index('a'))
+
+# tuples
+my_tup = ('One', 'Two', 'Three')
+
+# immutability
+# my_tup[0] = '1z' # TypeError: 'tuple' object does not support item assignment
