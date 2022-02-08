51d14ef10cb887b12d0de6c07d7155507200068c
Vaibhav Panvalkar <vpanvalkar@outlook.com>
2022-02-08 14:38:00 +0530

    sets

---
+++ Sets/set0.py
@@ -0,0 +1,13 @@
+# create set
+my_set = set()
+
+# add item to set
+my_set.add(1)
+my_set.add(1)
+my_set.add(2)
+
+print(my_set)
+
+# list casting to set
+my_list = [1,1,1,2,2,1,3,1]
+print(set(my_list))
